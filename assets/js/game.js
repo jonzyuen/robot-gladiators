435a9fde34959fc718c40f8ab1a96ded3e543c32
Jonathan Yuen <jonzyuen@gmail.com>
2021-06-23 21:21:17 -0700

    refined

--- assets/js/game.js
+++ assets/js/game.js
@@ -9,8 +9,13 @@ var enemyAttack = 12;
 
 // fight function
 var fight = function(enemyNames) {
+
   while(playerHealth > 0 && enemyHealth > 0) {
+
+  // ask fight or skip
   var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
+
+    // if skip, stop loop
     if (promptFight === "skip" || promptFight === "SKIP") {
 
     // skip confirmation
@@ -34,13 +39,13 @@ var fight = function(enemyNames) {
     // check enemy health
     if (enemyHealth <= 0) {
       window.alert(enemyNames + " has died!");
+      // win reward
       playerMoney = playerMoney + 20
       break;
     } else {
       window.alert(enemyNames + " still has " + enemyHealth + " health left.");
     }
 
-    // subtract 'enemyAttack' from 'playerHealth' and update 'playerHealth'
     playerHealth = playerHealth - enemyAttack;
     console.log(
       enemyNames + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
@@ -56,10 +61,21 @@ var fight = function(enemyNames) {
   }
 };
 
-// execute function
+// fight robot loop, one at a time
 for (var i = 0; i < enemyNames.length; i++) {
-  debugger;
+  // if player alive, keep fighting
+  if (playerHealth > 0) {
+    // round counter 
+    window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+   
+  // pick new enemy 
   var pickedEnemyName = enemyNames[i];
+  // reset enemy health 
   enemyHealth = 50;
   fight(pickedEnemyName);
+  } else {
+    // if player dead, stop game 
+    window.alert("You have lost your robot in battle! Game Over!");
+    break;
+  }
 }
