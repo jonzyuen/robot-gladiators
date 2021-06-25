b4ea1f60038a297a06d9c3a9deab9d49f25ff656
Jonathan Yuen <jonzyuen@gmail.com>
2021-06-24 18:31:13 -0700

    added high score system

--- assets/js/game.js
+++ assets/js/game.js
@@ -131,11 +131,21 @@ var startGame = function() {
 // end the game 
 var endGame = function() {
   window.alert("The game has now ended. Let's see how you did!");
-  // if player alive, player wins 
-  if (playerInfo.health > 0) {
-    window.alert("Great job, you've survived the game! You now have a score of " + playerInfo.money + ".");
+
+  // check locakStorage for high score, if none then use 0
+  var highScore = localStorage.getItem("highscore");
+  if (highScore === null) {
+    highScore = 0;
+  }
+
+  // if player has more money than high score, player has new high score 
+  if (playerInfo.money > highScore) {
+    localStorage.setItem("highscore", playerInfo.money);
+    localStorage.setItem("name", playerInfo.name);
+
+    alert(playerInfo.name + " now has the high score of " + playerInfo.money + "!");
   } else {
-    window.alert("You've lost your robot in battle.");
+    alert(playerInfo.name + " did not beat the high score of " + highscore + ". Maybe next time!");
   }
 
   // play again? 
